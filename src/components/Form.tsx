961995d4d16e720a105e0db59b9e89a8140b6ed4
= <=>
2024-10-04 12:50:15 -0300

    update

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react"
 import '../App.css'
-import {Sub} from '../../definitions'
+import { Sub } from '../../definitions'
 
 
-interface FormState{
-    inputValue:Sub
+interface FormState {
+    inputValue: Sub
 }
 
 interface FormProps {
     onNewSub: React.Dispatch<React.SetStateAction<Sub[]>>
 }
 
-const Form = ({onNewSub}:FormProps) => {
+const Form = ({ onNewSub }: FormProps) => {
     const [inputValue, setInputValue] = useState<FormState['inputValue']>({
         nick: "",
         subMoths: 0,
@@ -19,24 +19,37 @@ const Form = ({onNewSub}:FormProps) => {
         description: ""
     })
 
-//console.log(inputValue)
+    //console.log(inputValue)
 
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         onNewSub(subs => ([...subs, inputValue]))
+        handleClear()
     }
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setInputValue({ ...inputValue, [e.target.name]: e.target.value })
     }
+
+    const handleClear = () => {
+        setInputValue({
+            nick: "",
+            subMoths: 0,
+            avatar: "",
+            description: ""
+        });
+    }
+
+
     return (
         <>
             <form onSubmit={handleSubmit} className="form">
-                <input onChange={handleChange}  value={inputValue.nick} name='nick' placeholder="nick" />
-                <input onChange={handleChange}  value={inputValue.subMoths} name='subMoths' placeholder="subMoths" />
-                <input onChange={handleChange}  value={inputValue.avatar} name='avatar' placeholder="avatar" />
-                <textarea onChange={handleChange}  value={inputValue.description} name='description' placeholder="description" />
-                <button>Save new sub</button>
+                <input onChange={handleChange} value={inputValue.nick} name='nick' placeholder="nick" />
+                <input onChange={handleChange} value={inputValue.subMoths} name='subMoths' placeholder="subMoths" />
+                <input onChange={handleChange} value={inputValue.avatar} name='avatar' placeholder="avatar" />
+                <textarea onChange={handleChange} value={inputValue.description} name='description' placeholder="description" />
+                <button type="button" onClick={handleClear}>Clear</button>
+                <button type="submit">Save new sub</button>
             </form>
         </>
     )
